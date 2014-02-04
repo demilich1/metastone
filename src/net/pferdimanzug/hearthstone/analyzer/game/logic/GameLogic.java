8f8bbb9024f95193b8ce3fcd101a57b8260d838d
Manuel Ladebeck <xnemesisx@gmx.net>
2014-02-04 21:56:31 +0100

    - reverted 1 hp for heroes

--- src/net/pferdimanzug/hearthstone/analyzer/game/logic/GameLogic.java
+++ src/net/pferdimanzug/hearthstone/analyzer/game/logic/GameLogic.java
@@ -42,7 +42,7 @@ public class GameLogic implements IGameLogic {
 
 	public static final int MAX_MINIONS = 7;
 	public static final int MAX_HAND_CARDS = 10;
-	public static final int MAX_HERO_HP = 1;
+	public static final int MAX_HERO_HP = 30;
 	public static final int STARTER_CARDS = 3;
 	public static final int MAX_MANA = 10;
 
