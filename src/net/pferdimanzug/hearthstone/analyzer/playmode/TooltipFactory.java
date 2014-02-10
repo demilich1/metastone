c5970927501efbc6c7a80c00496f2d4b9d6da6e7
mladebeck <xnemesisx@gmx.net>
2014-02-10 17:48:30 +0100

    - added class for card tooltip

---
+++ src/net/pferdimanzug/hearthstone/analyzer/playmode/TooltipFactory.java
@@ -0,0 +1,14 @@
+package net.pferdimanzug.hearthstone.analyzer.playmode;
+
+import net.pferdimanzug.hearthstone.analyzer.game.cards.Card;
+import javafx.stage.Popup;
+
+public class TooltipFactory {
+	
+	private TooltipFactory() {}
+	
+	public static Popup createCardTooltip(Card card) {
+		
+	}
+
+}
