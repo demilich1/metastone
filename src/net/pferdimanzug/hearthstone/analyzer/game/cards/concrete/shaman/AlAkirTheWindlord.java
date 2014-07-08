aa4c1593fdec395d3448cb823c2ed878f38bd14a
mladebeck <xnemesisx@gmx.net>
2014-07-08 18:24:21 +0200

    - removed ununsed (because: slow) cloning lib - removed Swing UI lib - implemented Al'Akir the Windlord

---
+++ src/net/pferdimanzug/hearthstone/analyzer/game/cards/concrete/shaman/AlAkirTheWindlord.java
@@ -0,0 +1,21 @@
+package net.pferdimanzug.hearthstone.analyzer.game.cards.concrete.shaman;
+
+import net.pferdimanzug.hearthstone.analyzer.game.GameTag;
+import net.pferdimanzug.hearthstone.analyzer.game.cards.MinionCard;
+import net.pferdimanzug.hearthstone.analyzer.game.cards.Rarity;
+import net.pferdimanzug.hearthstone.analyzer.game.entities.heroes.HeroClass;
+import net.pferdimanzug.hearthstone.analyzer.game.entities.minions.Minion;
+
+public class AlAkirTheWindlord extends MinionCard {
+
+	public AlAkirTheWindlord() {
+		super("Al'Akir the Windlord", 3, 5, Rarity.LEGENDARY, HeroClass.SHAMAN, 8);
+		setDescription("Windfury, Charge, Divine Shield, Taunt");
+	}
+
+	@Override
+	public Minion summon() {
+		return createMinion(GameTag.WINDFURY, GameTag.CHARGE, GameTag.DIVINE_SHIELD, GameTag.TAUNT);
+	}
+
+}
