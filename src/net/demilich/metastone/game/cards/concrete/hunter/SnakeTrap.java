7c17c3a44ca51ab3b14085349b206882e32cc1f1
mladebeck <xnemesisx@gmx.net>
2015-04-13 15:56:20 +0200

    - implemented some class specific deathrattle cards

--- src/net/demilich/metastone/game/cards/concrete/hunter/SnakeTrap.java
+++ src/net/demilich/metastone/game/cards/concrete/hunter/SnakeTrap.java
@@ -2,7 +2,6 @@ package net.demilich.metastone.game.cards.concrete.hunter;
 
 import net.demilich.metastone.game.cards.Rarity;
 import net.demilich.metastone.game.cards.SecretCard;
-import net.demilich.metastone.game.cards.concrete.tokens.hunter.Snake;
 import net.demilich.metastone.game.entities.heroes.HeroClass;
 import net.demilich.metastone.game.spells.SummonSpell;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
@@ -13,8 +12,8 @@ public class SnakeTrap extends SecretCard {
 	public SnakeTrap() {
 		super("Snake Trap", Rarity.EPIC, HeroClass.HUNTER, 2);
 		setDescription("Secret: When one of your minions is attacked, summon three 1/1 Snakes.");
-		
-		SpellDesc summonSpell = SummonSpell.create(new Snake(), new Snake(), new Snake());
+		//SpellDesc summonSpell = SummonSpell.create(new Snake(), new Snake(), new Snake());
+		SpellDesc summonSpell = SummonSpell.create();
 		setTriggerAndEffect(new MinionAttacksTrigger(), summonSpell);
 	}
 	
