bebf4311b36140a929cfa026b109544a644bafa3
mladebeck <xnemesisx@gmx.net>
2015-11-12 15:41:01 +0100

    Fixed bugs which occured after merge
    - restored Misdirection to TargetAcquisitionTrigger - this is the only
    trigger at the moment which is early enough for target overrides.
    'PhysicalAttackTrigger' caused a lot of bugs with subsequent attacks or
    spells being redirected
    - changed Stablemaster to EPIC
    - fixed a bug where MindControlling while having a full board would not
    correctly remove spell triggers belonging to the destroyed minion
    - fixed a bug with Starving Buzzard on field + playing Desert Camel:
    there was an exception when Starving Buzzard drew the same card as
    summoned by the Camel

--- src/main/java/net/demilich/metastone/game/actions/ActionType.java
+++ src/main/java/net/demilich/metastone/game/actions/ActionType.java
@@ -1,5 +1,12 @@
 package net.demilich.metastone.game.actions;
 
 public enum ActionType {
-	SYSTEM, END_TURN, PHYSICAL_ATTACK, SPELL, SUMMON, HERO_POWER, BATTLECRY, EQUIP_WEAPON,
+	SYSTEM,
+	END_TURN,
+	PHYSICAL_ATTACK,
+	SPELL,
+	SUMMON,
+	HERO_POWER,
+	BATTLECRY,
+	EQUIP_WEAPON,
 }
