5095fe1254d026efa5d6b34aa4e26002c0c3acd4
Webley <webadict@gmail.com>
2016-01-03 17:07:17 -0600

    Fixed RecastSpell Recast Spell should no longer care about the filtering its targets.

--- src/main/java/net/demilich/metastone/game/spells/RecastSpell.java
+++ src/main/java/net/demilich/metastone/game/spells/RecastSpell.java
@@ -5,6 +5,7 @@ import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.cards.SpellCard;
 import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 
 public class RecastSpell extends Spell {
@@ -17,6 +18,7 @@ public class RecastSpell extends Spell {
 		}
 		if (card instanceof SpellCard) {
 			SpellCard spell = (SpellCard) card;
+			spell.setSpell(spell.getSpell().addArg(SpellArg.FILTER, null));
 			SpellUtils.castChildSpell(context, player, spell.getSpell(), source, target);
 		}
 	}
