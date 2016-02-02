2ac187a6315d96ee06392aca2aa1b5c99812ec39
Webley <webadict@gmail.com>
2016-02-02 15:57:47 -0600

    Fixed Summon Spells Summon spells should now try to find the correct position to summon to.

--- src/main/java/net/demilich/metastone/game/spells/SummonRandomMinionFromSpell.java
+++ src/main/java/net/demilich/metastone/game/spells/SummonRandomMinionFromSpell.java
@@ -8,10 +8,37 @@ import net.demilich.metastone.game.cards.CardCollection;
 import net.demilich.metastone.game.cards.CardType;
 import net.demilich.metastone.game.cards.MinionCard;
 import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.entities.minions.Minion;
+import net.demilich.metastone.game.entities.minions.RelativeToSource;
 import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 
 public class SummonRandomMinionFromSpell extends Spell {
+
+	private int getBoardPosition(GameContext context, Player player, SpellDesc desc, Entity source) {
+		final int UNDEFINED = -1;
+		int boardPosition = desc.getInt(SpellArg.BOARD_POSITION_ABSOLUTE, -1);
+		if (boardPosition != UNDEFINED) {
+			return boardPosition;
+		}
+		RelativeToSource relativeBoardPosition = (RelativeToSource) desc.get(SpellArg.BOARD_POSITION_RELATIVE);
+		if (relativeBoardPosition == null) {
+			return UNDEFINED;
+		}
+
+		int sourcePosition = context.getBoardPosition((Minion) source);
+		if (sourcePosition == UNDEFINED) {
+			return UNDEFINED;
+		}
+		switch (relativeBoardPosition) {
+		case LEFT:
+			return sourcePosition;
+		case RIGHT:
+			return sourcePosition + 1;
+		default:
+			return UNDEFINED;
+		}
+	}
 	
 	@Override
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
@@ -24,7 +51,7 @@ public class SummonRandomMinionFromSpell extends Spell {
 			}
 		}
 		
-		int boardPosition = desc.getInt(SpellArg.BOARD_POSITION_ABSOLUTE, -1);
+		int boardPosition = getBoardPosition(context, player, desc, source);
 		MinionCard minionCard = (MinionCard) relevantMinions.getRandom();
 		context.getLogic().summon(player.getId(), minionCard.summon(), null, boardPosition, false);
 	}
