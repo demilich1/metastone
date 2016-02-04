928f9acd50912ed254eb1ca376a4d64d49096e9c
Webley <webadict@gmail.com>
2016-02-04 17:52:34 -0600

    Removed Logger Info

--- src/main/java/net/demilich/metastone/game/spells/AddCardCostModifierSpell.java
+++ src/main/java/net/demilich/metastone/game/spells/AddCardCostModifierSpell.java
@@ -2,9 +2,6 @@ package net.demilich.metastone.game.spells;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.cards.Card;
@@ -17,8 +14,6 @@ import net.demilich.metastone.game.spells.desc.manamodifier.CardCostModifierDesc
 import net.demilich.metastone.game.targeting.EntityReference;
 
 public class AddCardCostModifierSpell extends Spell {
-	
-	Logger logger = LoggerFactory.getLogger(AddCardCostModifierSpell.class);
 
 	public static SpellDesc create(EntityReference target, CardCostModifier cardCostModifier) {
 		Map<SpellArg, Object> arguments = SpellDesc.build(AddCardCostModifierSpell.class);
@@ -34,12 +29,10 @@ public class AddCardCostModifierSpell extends Spell {
 	@Override
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		CardCostModifierDesc cardCostModifierDesc = (CardCostModifierDesc) desc.get(SpellArg.CARD_COST_MODIFIER);
-		logger.error(cardCostModifierDesc.toString());
 		if (cardCostModifierDesc.contains(CardCostModifierArg.TARGET)) {
 			Card card = (Card) context.resolveSingleTarget((EntityReference) cardCostModifierDesc.get(CardCostModifierArg.TARGET));
 			cardCostModifierDesc = cardCostModifierDesc.removeArg(CardCostModifierArg.TARGET);
 			cardCostModifierDesc = cardCostModifierDesc.addArg(CardCostModifierArg.ID, card.getId());
-			logger.error(cardCostModifierDesc.toString());
 		}
 		CardCostModifier cardCostModifier = cardCostModifierDesc.create();
 		cardCostModifier.setHost(target);
