c0db6d14a2e84dc441df635c52b704956f2bd2ed
Webley <webadict@gmail.com>
2016-03-15 19:59:24 -0500

    Added Comparison Condition

---
+++ src/main/java/net/demilich/metastone/game/spells/desc/condition/ComparisonCondition.java
@@ -0,0 +1,23 @@
+package net.demilich.metastone.game.spells.desc.condition;
+
+import net.demilich.metastone.game.GameContext;
+import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.spells.SpellUtils;
+import net.demilich.metastone.game.spells.desc.filter.Operation;
+
+public class ComparisonCondition extends Condition {
+
+	public ComparisonCondition(ConditionDesc desc) {
+		super(desc);
+	}
+
+	@Override
+	protected boolean isFulfilled(GameContext context, Player player, ConditionDesc desc, Entity target) {
+		int value1 = desc.getValue(ConditionArg.VALUE1, context, player, target, null, 0);
+		int value2 = desc.getValue(ConditionArg.VALUE2, context, player, target, null, 0);
+		Operation operation = (Operation) desc.get(ConditionArg.OPERATION);
+		return SpellUtils.evaluateOperation(operation, value1, value2);
+	}
+
+}
