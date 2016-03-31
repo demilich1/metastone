175b9ded65bdb78af09b8991136cb62957031ee5
Webley <webadict@gmail.com>
2016-03-31 15:00:55 -0500

    Added N'Zoth, the Corruptor
    Rewrote RessurectSpell to fit RessurectFromBothSpell. Allows for better
    customization.

--- src/main/java/net/demilich/metastone/game/spells/ResurrectSpell.java
+++ src/main/java/net/demilich/metastone/game/spells/ResurrectSpell.java
@@ -9,24 +9,35 @@ import net.demilich.metastone.game.cards.MinionCard;
 import net.demilich.metastone.game.entities.Entity;
 import net.demilich.metastone.game.entities.EntityType;
 import net.demilich.metastone.game.entities.minions.Minion;
+import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
+import net.demilich.metastone.game.spells.desc.filter.EntityFilter;
 
 public class ResurrectSpell extends Spell {
 
 	@Override
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		List<Minion> deadMinions = new ArrayList<>();
-		for (Entity deadEntity : player.getGraveyard()) {
+		EntityFilter cardFilter = (EntityFilter) desc.get(SpellArg.CARD_FILTER);
+		List<Entity> graveyard = new ArrayList<Entity>();
+		graveyard.addAll(player.getGraveyard());
+		for (Entity deadEntity : graveyard) {
 			if (deadEntity.getEntityType() == EntityType.MINION) {
-				deadMinions.add((Minion) deadEntity);
+				if (cardFilter == null || cardFilter.matches(context, player, deadEntity)) {
+					deadMinions.add((Minion) deadEntity);
+				}
 			}
 		}
-		if (deadMinions.isEmpty()) {
-			return;
+		int count = desc.getValue(SpellArg.VALUE, context, player, target, source, 1);
+		for (int i=0; i < count; i++) {
+			if (deadMinions.isEmpty()) {
+				return;
+			}
+			Minion resurrectedMinion = deadMinions.get(context.getLogic().random(deadMinions.size()));
+			MinionCard minionCard = (MinionCard) resurrectedMinion.getSourceCard();
+			context.getLogic().summon(player.getId(), minionCard.summon());
+			deadMinions.remove(resurrectedMinion);
 		}
-		Minion resurrectedMinion = deadMinions.get(context.getLogic().random(deadMinions.size()));
-		MinionCard minionCard = (MinionCard) resurrectedMinion.getSourceCard();
-		context.getLogic().summon(player.getId(), minionCard.summon());
 	}
 
 }
