33d56f19ae7f88d72ed8aa5bb016badc44ce1c9c
Webley <webadict@gmail.com>
2016-04-08 09:03:08 -0500

    Yogg-Saron is truly random
    "I'M the one in control here!"

--- src/main/java/net/demilich/metastone/game/spells/custom/YoggSaronSpell.java
+++ src/main/java/net/demilich/metastone/game/spells/custom/YoggSaronSpell.java
@@ -1,11 +1,18 @@
 package net.demilich.metastone.game.spells.custom;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.actions.BattlecryAction;
+import net.demilich.metastone.game.actions.GameAction;
+import net.demilich.metastone.game.behaviour.IBehaviour;
+import net.demilich.metastone.game.behaviour.PlayRandomBehaviour;
 import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.cards.CardCatalogue;
 import net.demilich.metastone.game.cards.CardCollection;
@@ -20,6 +27,8 @@ import net.demilich.metastone.game.statistics.Statistic;
 import net.demilich.metastone.game.targeting.TargetSelection;
 
 public class YoggSaronSpell extends Spell {
+	
+	Logger logger = LoggerFactory.getLogger(YoggSaronSpell.class);
 
 	public static SpellDesc create() {
 		Map<SpellArg, Object> arguments = SpellDesc.build(YoggSaronSpell.class);
@@ -30,33 +39,52 @@ public class YoggSaronSpell extends Spell {
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		// This spell is crazy.
 		CardCollection spellCards = CardCatalogue.query(context.getDeckFormat(), CardType.SPELL);
-		
 		// Straight up insane.
+		
+		// Set behavior to random. Because we're already insane.
+		// This allows Discover effects and targeting to actually be random.
+		IBehaviour currentBehaviour = player.getBehaviour();
+		player.setBehaviour(new PlayRandomBehaviour());
+		// HAHAHAHAHAHAHAHAHAHA!
 		for (int i = 0; i <= (int) player.getStatistics().getLong(Statistic.SPELLS_CAST); i++) {
-				Card randomCard = spellCards.getRandom();
-				if (randomCard instanceof ChooseOneCard) {
-					ChooseOneCard chooseOneCard = (ChooseOneCard) randomCard;
-					Card[] cards = chooseOneCard.getChoiceCards();
-					randomCard = cards[context.getLogic().random(cards.length)];
-				}
-				SpellCard spellCard = (SpellCard) randomCard;
-				if (spellCard.canBeCast(context, player)) {
-					SpellCard copyCard = spellCard.clone();
-					context.getLogic().drawSetAsideCard(player.getId(), copyCard);
-					
-					BattlecryAction battlecry = BattlecryAction.createBattlecry(copyCard.getSpell(), copyCard.getTargetRequirement());
-					battlecry.setSource(copyCard.getReference());
-					if (battlecry.getTargetRequirement() != TargetSelection.NONE) {
-						List<Entity> validTargets = context.getLogic().getValidTargets(player.getId(), battlecry);
-						if (validTargets.isEmpty()) {
-							continue;
-						}
-						
-						battlecry.setTarget(validTargets.get(context.getLogic().random(validTargets.size())));
+			Card randomCard = spellCards.getRandom();
+			logger.debug("Yogg-Saron chooses to play " + randomCard.getName());
+			if (randomCard instanceof ChooseOneCard) {
+				ChooseOneCard chooseOneCard = (ChooseOneCard) randomCard;
+				Card[] cards = chooseOneCard.getChoiceCards();
+				randomCard = cards[context.getLogic().random(cards.length)];
+			}
+			SpellCard spellCard = (SpellCard) randomCard;
+			if (spellCard.canBeCast(context, player)) {
+				SpellCard copyCard = spellCard.clone();
+				context.getLogic().drawSetAsideCard(player.getId(), copyCard);
+
+				BattlecryAction battlecry = BattlecryAction.createBattlecry(copyCard.getSpell(), copyCard.getTargetRequirement());
+				GameAction battlecryAction = null;
+				battlecry.setSource(copyCard.getReference());
+				if (battlecry.getTargetRequirement() != TargetSelection.NONE) {
+					List<Entity> validTargets = context.getLogic().getValidTargets(player.getId(), battlecry);
+					if (validTargets.isEmpty()) {
+						continue;
 					}
-					context.getLogic().performGameAction(player.getId(), battlecry);
+
+					List<GameAction> battlecryActions = new ArrayList<>();
+					for (Entity validTarget : validTargets) {
+						GameAction targetedBattlecry = battlecry.clone();
+						targetedBattlecry.setTarget(validTarget);
+						battlecryActions.add(targetedBattlecry);
+					}
+
+					battlecryAction = player.getBehaviour().requestAction(context, player, battlecryActions);
+				} else {
+					battlecryAction = battlecry;
 				}
+				context.getLogic().performGameAction(player.getId(), battlecryAction);
+			}
 		}
+		
+		player.setBehaviour(currentBehaviour);
+		// *ahem* Back to normal.
 	}
 
 }
