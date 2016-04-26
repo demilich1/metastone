f923d545a7d30c5b5cf58a6c49d9ec1472e59ab8
Webley <webadict@gmail.com>
2016-04-25 21:31:20 -0500

    Fixed Swipe

--- src/main/java/net/demilich/metastone/game/spells/custom/SwipeSpell.java
+++ src/main/java/net/demilich/metastone/game/spells/custom/SwipeSpell.java
@@ -12,7 +12,7 @@ public class SwipeSpell extends Spell {
 
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		int primaryDamage = desc.getValue(SpellArg.VALUE, context, player, target, source, 2);
-		int secondaryDamage = desc.getValue(SpellArg.VALUE, context, player, target, source, 1);
+		int secondaryDamage = desc.getValue(SpellArg.SECONDARY_VALUE, context, player, target, source, 1);
 		for (Actor character : context.getOpponent(player).getCharacters()) {
 			int damage = character == target ? primaryDamage : secondaryDamage;
 			context.getLogic().damage(player, character, damage, source);
