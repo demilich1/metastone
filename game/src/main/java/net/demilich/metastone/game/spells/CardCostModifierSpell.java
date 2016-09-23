91f01c9be4fbd1908cc3e098115af8deea20a073
Webley <webadict@gmail.com>
2016-09-23 12:08:33 -0500

    Fixed CardCostModifierSpell
    - Updated CardCostModifierSpell create() methods.
    - Fixed some rare crashes.

--- game/src/main/java/net/demilich/metastone/game/spells/CardCostModifierSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/CardCostModifierSpell.java
@@ -7,32 +7,47 @@ import java.util.Map;
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.cards.Card;
+import net.demilich.metastone.game.cards.costmodifier.CardCostModifier;
 import net.demilich.metastone.game.entities.Entity;
 import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 import net.demilich.metastone.game.spells.desc.filter.EntityFilter;
 import net.demilich.metastone.game.spells.desc.manamodifier.CardCostModifierArg;
 import net.demilich.metastone.game.spells.desc.manamodifier.CardCostModifierDesc;
+import net.demilich.metastone.game.spells.desc.valueprovider.AlgebraicOperation;
 import net.demilich.metastone.game.targeting.EntityReference;
 
 public class CardCostModifierSpell extends Spell {
 
 	public static SpellDesc create(CardCostModifierDesc cardCostModifierDesc) {
-		return create(null, cardCostModifierDesc);
+		return create(cardCostModifierDesc, null);
 	}
 
-	public static SpellDesc create(EntityReference target, CardCostModifierDesc cardCostModifierDesc) {
+	public static SpellDesc create(CardCostModifierDesc cardCostModifierDesc, EntityFilter cardFilter) {
 		Map<SpellArg, Object> arguments = SpellDesc.build(CardCostModifierSpell.class);
 		arguments.put(SpellArg.CARD_COST_MODIFIER, cardCostModifierDesc);
-		arguments.put(SpellArg.TARGET, target);
+		arguments.put(SpellArg.CARD_FILTER, cardFilter);
 		return new SpellDesc(arguments);
+	}
+
+	public static SpellDesc create(EntityReference target, AlgebraicOperation operation, int value) {
+		return CardCostModifierSpell.create(target, operation, value, null);
+	}
+
+
+	public static SpellDesc create(EntityReference target, AlgebraicOperation operation, int value, EntityFilter cardFilter) {
+		CardCostModifierDesc manaModifierDesc = new CardCostModifierDesc(CardCostModifierDesc.build(CardCostModifier.class));
+		manaModifierDesc = manaModifierDesc.addArg(CardCostModifierArg.OPERATION, operation);
+		manaModifierDesc = manaModifierDesc.addArg(CardCostModifierArg.TARGET, target);
+		manaModifierDesc = manaModifierDesc.addArg(CardCostModifierArg.VALUE, value);
+		return CardCostModifierSpell.create(manaModifierDesc, cardFilter);
 	}
 
 	@Override
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		CardCostModifierDesc manaModifierDesc = (CardCostModifierDesc) desc.get(SpellArg.CARD_COST_MODIFIER);
 		EntityFilter cardFilter = (EntityFilter) desc.get(SpellArg.CARD_FILTER);
-		if (manaModifierDesc.contains(CardCostModifierArg.TARGET)) {
+		if (manaModifierDesc.get(CardCostModifierArg.TARGET) != null) {
 			// First, resolve the targets, so that you can get the current cards this affects.
 			// This spell SPECIFICALLY targets cards, even if those cards would change. So,
 			// targeting FRIENDLY_HAND would pull cards in the hand NOW, as opposed to cards
@@ -54,6 +69,9 @@ public class CardCostModifierSpell extends Spell {
 			List<Integer> cardIds = new ArrayList<Integer>();
 			cardIds.add(target.getId());
 			manaModifierDesc = manaModifierDesc.addArg(CardCostModifierArg.CARD_IDS, cardIds);
+		}
+		if (target == null) {
+			target = player;
 		}
 		context.getLogic().addManaModifier(player, manaModifierDesc.create(), target);
 	}
