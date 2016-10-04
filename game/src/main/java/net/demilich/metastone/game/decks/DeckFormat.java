98723e6e96634ba090f13bb3a2a449d0c399239b
Webley <webadict@gmail.com>
2016-10-04 14:18:19 -0500

    Reformated DeckFormat

--- game/src/main/java/net/demilich/metastone/game/decks/DeckFormat.java
+++ game/src/main/java/net/demilich/metastone/game/decks/DeckFormat.java
@@ -20,20 +20,28 @@ public class DeckFormat {
 		sets.add(cardSet);
 	}
 
-	public boolean inSet(Deck deck) {
+	public boolean isInFormat(Card card) {
+		if (sets.contains(card.getCardSet())) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isInFormat(CardSet set) {
+		return sets.contains(set);
+	}
+
+	public boolean isInFormat(Deck deck) {
 		for (Card card : deck.getCards()) {
-			if (!sets.contains(card.getCardSet())) {
+			if (!isInFormat(card)) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public boolean inSet(Card card) {
-		if (sets.contains(card.getCardSet())) {
-			return true;
-		}
-		return false;
+	public List<CardSet> getCardSets() {
+		return new ArrayList<CardSet>(sets);
 	}
 
 	public String getName() {
