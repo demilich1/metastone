554e53438204c08e3d49e9149c75b3a54460ecd7
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2016-10-18 02:18:41 -0400

    Test some more functions in matchmaking

--- net/src/main/java/com/hiddenswitch/proto3/net/models/MatchmakingResponse.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/models/MatchmakingResponse.java
@@ -3,7 +3,40 @@ package com.hiddenswitch.proto3.net.models;
 import net.demilich.metastone.game.GameContext;
 
 public class MatchmakingResponse {
-	public MatchmakingRequestRetry retry;
-	public String myChannelId;
-	public Game game;
+	private MatchmakingRequestRetry retry;
+	private String myChannelId;
+	private Game game;
+	private String gameId;
+
+	public MatchmakingRequestRetry getRetry() {
+		return retry;
+	}
+
+	public void setRetry(MatchmakingRequestRetry retry) {
+		this.retry = retry;
+	}
+
+	public String getMyChannelId() {
+		return myChannelId;
+	}
+
+	public void setMyChannelId(String myChannelId) {
+		this.myChannelId = myChannelId;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public String getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(String gameId) {
+		this.gameId = gameId;
+	}
 }
