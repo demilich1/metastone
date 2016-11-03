d17cda6b35c7e75ff3d9f6dedb4d1aaed225f354
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2016-11-02 21:13:20 -0400

    Name the game Minionate

--- app/src/main/java/net/demilich/metastone/MetaStone.java
+++ app/src/main/java/net/demilich/metastone/MetaStone.java
@@ -35,7 +35,7 @@ public class MetaStone extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Proto3");
+		primaryStage.setTitle("Minionate");
 		primaryStage.initStyle(StageStyle.UNIFIED);
 		primaryStage.setResizable(false);
 		primaryStage.getIcons().add(new Image(IconFactory.getImageUrl("ui/app_icon.png")));
