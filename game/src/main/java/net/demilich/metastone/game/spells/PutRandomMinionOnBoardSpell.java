f4221bd27799df2d8c0c74606dbda6e95f101303
Webley <webadict@gmail.com>
2016-11-09 15:30:27 -0600

    Card Fixes
    - Fixed Blood of The Ancient One to destroy itself when merging.
    - Fixed Meanstreet Marshal to allow it to check if it could draw a card.
    - Fixed PutRandomMinionOnBoardSpell from giving MetaStone a heart attack
    when it put a minion with a spell trigger on the board.

--- game/src/main/java/net/demilich/metastone/game/spells/PutRandomMinionOnBoardSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/PutRandomMinionOnBoardSpell.java
@@ -42,14 +42,16 @@ public class PutRandomMinionOnBoardSpell extends Spell {
 		// we need to remove the card temporarily here, because there are card interactions like Starving Buzzard + Desert Camel
 		// which could result in the card being drawn while a minion is summoned
 		if (cardLocation == CardLocation.DECK) {
-			player.getDeck().remove(minionCard);	
+			player.getDeck().remove(minionCard);
+			player.getSetAsideZone().add(minionCard);
 		}
 		
 		boolean summonSuccess = context.getLogic().summon(player.getId(), minionCard.summon());
 		
 		// re-add the card here if we removed it before
 		if (cardLocation == CardLocation.DECK) {
-			player.getDeck().add(minionCard);	
+			player.getSetAsideZone().remove(minionCard);
+			player.getDeck().add(minionCard);
 		}
 		
 		if (summonSuccess) {
--- game/src/main/java/net/demilich/metastone/game/spells/custom/MergeSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/custom/MergeSpell.java
@@ -7,7 +7,10 @@ import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.entities.Entity;
 import net.demilich.metastone.game.entities.minions.Minion;
+import net.demilich.metastone.game.spells.DestroySpell;
+import net.demilich.metastone.game.spells.ForceDeathPhaseSpell;
 import net.demilich.metastone.game.spells.Spell;
+import net.demilich.metastone.game.spells.SpellUtils;
 import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 import net.demilich.metastone.game.targeting.EntityReference;
@@ -22,11 +25,15 @@ public class MergeSpell extends Spell {
 	@Override
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		List<Entity> entities = context.resolveTarget(player, source, EntityReference.OTHER_FRIENDLY_MINIONS);
+		SpellDesc destroySpell = DestroySpell.create(target.getReference());
+		SpellUtils.castChildSpell(context, player, destroySpell, source, target);
 		String cardId = desc.getString(SpellArg.CARD);
 		for (Entity entity : entities) {
 			Minion minion = (Minion) entity;
 			if (minion.getSourceCard().getCardId().equalsIgnoreCase(cardId)) {
-				context.getLogic().transformMinion(minion, null);
+				destroySpell = DestroySpell.create(minion.getReference());
+				SpellUtils.castChildSpell(context, player, destroySpell, source, target);
+				SpellUtils.castChildSpell(context, player, ForceDeathPhaseSpell.create(), source, target);
 				return;
 			}
 		}
