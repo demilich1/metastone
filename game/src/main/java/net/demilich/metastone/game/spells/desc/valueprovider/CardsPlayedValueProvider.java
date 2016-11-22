8737c3ffd7eb88dcbcafac1f13ec9e37dfd77e00
Webley <webadict@gmail.com>
2016-11-22 14:27:28 -0600

    Added Kabal Crystal Runner
    - Added CardsPlayedValueProvider to allow the search of cards played to
    get values.
    - Updated MinionSummonValueProvider to use new context cardID search.
    - Text fixes.

--- game/src/main/java/net/demilich/metastone/game/spells/desc/valueprovider/CardsPlayedValueProvider.java
+++ game/src/main/java/net/demilich/metastone/game/spells/desc/valueprovider/CardsPlayedValueProvider.java
@@ -7,21 +7,21 @@ import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.entities.Entity;
 import net.demilich.metastone.game.spells.desc.filter.EntityFilter;
 
-public class MinionSummonValueProvider extends ValueProvider {
+public class CardsPlayedValueProvider extends ValueProvider {
 
-	public MinionSummonValueProvider(ValueProviderDesc desc) {
+	public CardsPlayedValueProvider(ValueProviderDesc desc) {
 		super(desc);
 	}
 
 	@Override
 	protected int provideValue(GameContext context, Player player, Entity target, Entity host) {
-		Map<String, Integer> minionIds = player.getStatistics().getMinionsSummoned();
+		Map<String, Integer> cardIds = player.getStatistics().getCardsPlayed();
 		int count = 0;
 		EntityFilter filter = (EntityFilter) desc.get(ValueProviderArg.FILTER);
-		for (String minionId : minionIds.keySet()) {
-			Entity entity = context.getCardById(minionId);
+		for (String cardId : cardIds.keySet()) {
+			Entity entity = context.getCardById(cardId);
 			if (filter == null || filter.matches(context, player, entity)) {
-				count += minionIds.get(minionId);
+				count += cardIds.get(cardId);
 			}
 		}
 		return count;
