fe403b89497c6628732dc22ddbcaabe977a516b0
Webley <webadict@gmail.com>
2016-11-29 18:32:07 -0600

    Added remaining cards

---
+++ game/src/main/java/net/demilich/metastone/game/spells/RandomSpellTargetSpell.java
@@ -0,0 +1,28 @@
+package net.demilich.metastone.game.spells;
+
+import java.util.List;
+
+import net.demilich.metastone.game.Environment;
+import net.demilich.metastone.game.GameContext;
+import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.actions.PlaySpellCardAction;
+import net.demilich.metastone.game.cards.SpellCard;
+import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.spells.desc.SpellDesc;
+
+public class RandomSpellTargetSpell extends Spell {
+
+	@Override
+	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
+		if (!(target instanceof SpellCard)) {
+			System.out.println("Not a spell");
+			return;
+		}
+		SpellCard spellCard = (SpellCard) target;
+		PlaySpellCardAction action = new PlaySpellCardAction(spellCard.getSpell(), spellCard, spellCard.getTargetRequirement());
+		
+		List<Entity> targets = context.getLogic().getValidTargets(context.getActivePlayerId(), action);
+		Entity randomTarget = targets.get(context.getLogic().random(targets.size()));
+		context.getEnvironment().put(Environment.TARGET_OVERRIDE, randomTarget.getReference());
+	}
+}
