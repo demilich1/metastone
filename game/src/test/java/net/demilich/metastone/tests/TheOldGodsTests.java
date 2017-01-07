fc685982f6b7dfcea248ffa408da31c352415d89
Sohan Jain <sj346@cornell.edu>
2017-01-07 15:07:01 -0500

    add murloc razorgill and update call in the finishers

---
+++ game/src/test/java/net/demilich/metastone/tests/TheOldGodsTests.java
@@ -0,0 +1,26 @@
+package net.demilich.metastone.tests;
+
+
+import net.demilich.metastone.game.GameContext;
+import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.cards.CardCatalogue;
+import net.demilich.metastone.game.entities.heroes.HeroClass;
+import net.demilich.metastone.game.entities.minions.Minion;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TheOldGodsTests extends TestBase {
+
+	@Test
+	public void testCallInTheFinishers() {
+		GameContext context = createContext(HeroClass.SHAMAN, HeroClass.WARRIOR);
+		Player player = context.getPlayer1();
+
+		playCard(context, player, CardCatalogue.getCardById("spell_call_in_the_finishers"));
+
+		for (Minion minion : player.getMinions()) {
+			Assert.assertEquals(minion.getSourceCard().getCardId(), "token_murloc_razorgill");
+		}
+	}
+
+}
