66e200f043712ade43445c77c49942f43eeaec8d
Sohan Jain <sj346@cornell.edu>
2017-01-07 15:51:13 -0500

    add test for darkshire councilman

--- game/src/test/java/net/demilich/metastone/tests/TheOldGodsTests.java
+++ game/src/test/java/net/demilich/metastone/tests/TheOldGodsTests.java
@@ -4,6 +4,7 @@ package net.demilich.metastone.tests;
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.cards.CardCatalogue;
+import net.demilich.metastone.game.cards.MinionCard;
 import net.demilich.metastone.game.entities.heroes.HeroClass;
 import net.demilich.metastone.game.entities.minions.Minion;
 import org.testng.Assert;
@@ -21,6 +22,27 @@ public class TheOldGodsTests extends TestBase {
 		for (Minion minion : player.getMinions()) {
 			Assert.assertEquals(minion.getSourceCard().getCardId(), "token_murloc_razorgill");
 		}
+	}
+
+	@Test
+	public void testDarkshireCoucilman() {
+		GameContext context = createContext(HeroClass.SHAMAN, HeroClass.WARRIOR);
+		Player player = context.getPlayer1();
+		Player opponent = context.getOpponent(player);
+
+		Minion darkshireCouncilman = playMinionCard(context, player, (MinionCard) CardCatalogue.getCardById("minion_darkshire_councilman"));
+		Assert.assertEquals(darkshireCouncilman.getAttack(), darkshireCouncilman.getBaseAttack());
+
+		Minion darkshireCouncilman2 = playMinionCard(context, player, (MinionCard) CardCatalogue.getCardById("minion_darkshire_councilman"));
+		Assert.assertEquals(darkshireCouncilman.getAttack(), darkshireCouncilman.getBaseAttack() + 1);
+		Assert.assertEquals(darkshireCouncilman2.getAttack(), darkshireCouncilman2.getBaseAttack());
+
+		context.getLogic().endTurn(player.getId());
+		Minion opponentMinion = playMinionCard(context, opponent, (MinionCard) CardCatalogue.getCardById("minion_darkshire_councilman"));
+
+		Assert.assertEquals(darkshireCouncilman.getAttack(), darkshireCouncilman.getBaseAttack() + 1);
+		Assert.assertEquals(darkshireCouncilman2.getAttack(), darkshireCouncilman2.getBaseAttack());
+		Assert.assertEquals(opponentMinion.getAttack(), opponentMinion.getBaseAttack());
 	}
 
 }
