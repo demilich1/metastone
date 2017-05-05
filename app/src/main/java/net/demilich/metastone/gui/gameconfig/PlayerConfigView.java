1a0236df2cc82fe482cfd5100cf61a51694bc7ca
Boris <doux.boris@laposte.net>
2017-05-05 15:39:53 +0200

    add flatMonteCarlo behaviour

--- app/src/main/java/net/demilich/metastone/gui/gameconfig/PlayerConfigView.java
+++ app/src/main/java/net/demilich/metastone/gui/gameconfig/PlayerConfigView.java
@@ -23,6 +23,7 @@ import net.demilich.metastone.game.behaviour.PlayRandomBehaviour;
 import net.demilich.metastone.game.behaviour.heuristic.WeightedHeuristic;
 import net.demilich.metastone.game.behaviour.human.HumanBehaviour;
 import net.demilich.metastone.game.behaviour.threat.GameStateValueBehaviour;
+import net.demilich.metastone.game.behaviour.FlatMonteCarlo;
 import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.cards.CardCatalogue;
 import net.demilich.metastone.game.cards.HeroCard;
@@ -169,6 +170,7 @@ public class PlayerConfigView extends VBox {
 
 		behaviourList.add(new GreedyOptimizeMove(new WeightedHeuristic()));
 		behaviourList.add(new NoAggressionBehaviour());
+		behaviourList.add(new FlatMonteCarlo(100));
 
 		behaviourBox.setItems(behaviourList);
 		behaviourBox.valueProperty().addListener(this::onBehaviourChanged);
